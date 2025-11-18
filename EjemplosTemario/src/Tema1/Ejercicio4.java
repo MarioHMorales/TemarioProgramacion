adf70c497510d9fc239bfaeaf37f97391ed726c8
MarioHMorales <mario.1989hm@gmail.com>
2025-11-18 21:50:45 +0100

    18/11/25

---
+++ EjemplosTemario/src/Tema1/Ejercicio4.java
@@ -0,0 +1,15 @@
+
+
+package Tema1;
+
+/**
+ *
+ * @author Hinojos Morales, Mario
+ * 
+ * Escribe un programa en Java que solicite la altura y la longitud del lado (en metros) de una pirámide de base cuadrángular y calcule su volumen en metros cúbicos.
+
+Después de realizar ese cálculo, muestra a cuántas piscinas olímpicas equivale dicho volumen, teniendo en cuenta que una piscina olímpica tiene 2500 metros cúbicos.
+ */
+public class Ejercicio4 {
+
+}
